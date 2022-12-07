f3d441c79ba303770e02e6b700f360eb968c3abd
Fuad303 <sadiqovfuad8@gmail.com>
2022-12-08 00:39:26 +0400

    some possibly meaningful message

--- nav.js
+++ nav.js
@@ -68,3 +68,21 @@ document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
 }
 // End of Top Function
+
+// Language 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
